1684ec649dbcf4102eb6fbb35e96ca07a0c7192f
linhvuthuy2901 <24021548@vnu.edu.vn>
2025-03-12 14:03:01 +0700

    INT2215_7 24021548 Vu Thuy Linh

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -14,7 +14,7 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    return rand()%100+1;
 }
 
 
@@ -26,8 +26,10 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int x;
+    cout<<"Nhap vao mot so";
+    cin>>x;
+    return x;
 }
 
 
@@ -46,7 +48,18 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-
+    if(randomNumber<number)
+    {
+        answer= "Your number is higher.";
+    }
+    else if(randomNumber>number)
+    {
+        answer= "Your number is lower.";
+    }
+    else 
+    {
+        answer="Congratulation! You win.";
+    }
     return answer;
 }
 
@@ -59,7 +72,10 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
+    if(answer=="Your number is higher."||answer=="Your number is lower.")
+    {
+        return false;
+    }
     return true;
 }
 
@@ -73,7 +89,10 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    if(isContinued=='Y'||isContinued=='y')
+    {
+        result=true;
+    }
     return result;
 }
 
@@ -87,7 +106,8 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cout<<"Ban co muon tiep tuc khong?Nhap 'y' hoac 'Y' de tiep tuc :";
+    cin>>isContinued;
     return isContinued;
 }
 
